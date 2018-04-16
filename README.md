b5993a0eba34244251cb6728e86d94317bc6bdb0
Marcio Rodrigues <mafideju@outlook.com>
2018-04-16 13:37:01 -0300

    Foundation Stone

---
+++ README.md
@@ -0,0 +1,19 @@
+
+Arquivos criados sob a instrução de Brad Traversy da Traversy Media para o curso de 
+'Bootstrap 4 from Scratch' na 'Udemy.com'.
+
+Entre em contato através do email >> mafideju@outlook.com  <<
+
+Todo o código foi digitado de acordo com as instruções dadas em aula, usando lorem ipsum e idioma inglês, 
+posteriormente traduzidos para português com adição de textos e imagens livres.
+
+
+
+
+Files developed following instructions of Mr. Brad Traversy from Traversy Media for the 
+'Bootstrap 4 from Scractch' @'Udemy.com' course.
+
+Feel free to contact me anytime in the adress above.
+
+Every line in this code was typed following lectures instructions using lorem ipsum and english language,
+later translated into Portuguese with the addition of free stock texts and images.
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+
+// FUNÇÃO COMPILADOR DE SASS E INJETOR NO BROWSER
+gulp.task('sass', function(){
+	return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+	.pipe(sass())
+	.pipe(gulp.dest("src/css"))
+	.pipe(browserSync.stream());
+});
+
+// TIRAR ARQUIVOS JS DO NODE_MODULES E MOVER PARA A PASTA FONTE DO JS
+gulp.task('js', function(){
+	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
+	.pipe(gulp.dest("src/js"))
+	.pipe(browserSync.stream());
+});
+
+//FAZER O SASS E O SERVIDOR FUNCIONAREM - APONTAR UMA FUNÇÃO PARA QUE SEMPRE COMPILE OS ARQUIVOS
+gulp.task('serve', ['sass'], function(){
+	browserSync.init({
+		server: "./src"
+	});
+	gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+	gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+//MOVER A PASTA FONTES PARA A PASTA DE TRABALHO (SRC)
+gulp.task('fonts', function(){
+	return gulp.src('node_modules/font-awesome/fonts/*')
+	.pipe(gulp.dest("src/fonts"));
+});
+
+//MOVER A PASTA FONTAWESOME PARA A PASTA DE TRABALHO (SRC)
+gulp.task('fa', function(){
+	return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+	.pipe(gulp.dest("src/css"));
+});
+
+ gulp.task('default', ['js','serve','fa','fonts']);
